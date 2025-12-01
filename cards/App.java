264283a6ccba169d4d1a411776d75c0049a8f898
josh461 <joshharman462@gmail.com>
2025-12-01 02:13:17 +0000

    Done

--- cards/App.java
+++ cards/App.java
@@ -1,16 +1,12 @@
 package cards;
 
 public class App {
+
     public static void main(String[] args) {
-        Deck d = new Deck();
-        d.print(5);
 
-        Card c = new Card(0, 10);
-        System.out.println(c);
+        CardTable table = new CardTable();
 
-        Hand h = new Hand(5);
-        h.add(new Card(3, 12));
-        h.add(new Card(2, 0));
-        System.out.println(h);
+        table.startPlaying();
     }
+
 }
