365f587ad153cd623a5176afebd5ea5b97697457
josh461 <joshharman462@gmail.com>
2026-04-12 14:47:37 -0700

    Completed
    I apologize for the late submission, I wasn't able to finish it before the deadline and then I got sick throughout half of my Spring Break. This was all done via my local PC on VS Code.

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -1,23 +1,24 @@
 package tower;
 
 public class TowerSolver {
-    private TowerModel model;
+   private TowerModel model;
 
-    public TowerSolver()
-    {
-        // Nothing to do here
-    }
+   public TowerSolver() {
+   }
 
-    public void solve(TowerModel model)
-    {
-        this.model = model;
-        // Call the missing solve method (not this one)
-    }
+   public void solve(TowerModel var1) {
+      this.model = var1;
+      this.solve(var1.height(), 0, 2, 1);
+   }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
+   private void solve(int var1, int var2, int var3, int var4) {
+      if (var1 == 1) {
+         this.model.move(var2, var3);
+      } else {
+         this.solve(var1 - 1, var2, var4, var3);
+         this.model.move(var2, var3);
+         this.solve(var1 - 1, var4, var3, var2);
+      }
 
+   }
 }
